de63e9bcf44d013a9ef38c7af2f828448de201ae
Torao-Law <dandisyahrullah.12@gmail.com>
2023-09-22 15:07:36 +0700

    first commit

--- index.ts
+++ index.ts
@@ -1,46 +1,15 @@
-import express, { Request, Response } from "express"
-import ITodos from "./src/interface/Todos"
-import Todos from "./src/mocks/Todos"
-
-const app = express()
-const PORT = 5000
-
-app.use(express.json())
-
-app.get("/", (req: Request, res: Response) : Response => {
-  return res.status(200).json({ message: "Gagal biasa menyerah jangan pernah" })
-})
-
-app.get("/todos", (req: Request, res: Response) : Response => {
-  return res.status(200).json({ data: Todos})
-})
-
-// localhost:5000/todos/1
-// 1 = string => number (Number() / parseInte() )
-
-app.get("/todo/:id", (req: Request, res: Response) : Response => {
-  const id = Number(req.params.id)
-  const data = Todos.find((data) => data.id === id)
-  
-  return res.status(200).json(data)
-})
-
-app.post('/todo',(req:Request, res:Response) : Response => {
-  const data: ITodos = req.body
-  Todos.push(data)
-
-  return res.status(200).json({ data: Todos})
-})
-
-app.delete("/todo/:id", (req: Request, res: Response) : Response => {
-  const { id } = req.params
-  const data: ITodos[] = Todos.filter(todo => todo.id !== parseInt(id))
-
-  return res.status(200).json(data)
-})
+import express from "express"
+import router from './src/routes'
 
 async function start(): Promise<void> {
   try {
+    const app = express()
+    const PORT = 5000
+
+    app.use(express.json())
+
+    app.use("/api/v1", router)
+    
     app.listen(PORT, () => console.log("Server express running"))
   } catch (error) {
     console.log(error)
@@ -49,3 +18,6 @@ async function start(): Promise<void> {
 }
 
 void start()
+
+
+// http:localhost:5000/api/v1/todos
---
+++ src/service/TodoServices.ts
@@ -0,0 +1,72 @@
+import { Request, Response } from "express";
+import ITodos from "../interface/Todos";
+import Todos from "../mocks/Todos";
+
+export default new class TodoService {
+  private todos: ITodos[]
+
+  constructor() {
+    this.todos = [ ...Todos ]
+  }
+
+  find(req: Request, res: Response) : Response {
+    try {
+      return res.status(200).json(this.todos)
+    } catch (error) {
+      return res.status(500).json({ error: "Internal Server Error"})
+    }
+  }
+
+  findOne(req: Request, res: Response) : Response {
+    try {
+      const id = Number(req.params.id)
+      const data = Todos.find((data) => data.id === id)
+
+      return res.status(200).json(data)
+    } catch (error) {
+      return res.status(500).json({ message: "Something error while findOne"})
+    }
+  }
+
+  create(req:Request, res:Response) : Response {
+    try {
+      const data: ITodos = req.body
+      Todos.push(data)
+
+      return res.status(200).json({ data: Todos})
+    } catch (error) {
+      return res.status(500).json({ message: "Something error while create todo" })
+    }
+  }
+
+  update(req: Request, res: Response) : Response {
+    try {
+      const id: number = Number(req.params.id)
+      const updateTodo: ITodos = req.body
+
+      const index: number = this.todos.findIndex(todo => todo.id === id)
+      
+      if(index !== -1) {
+        this.todos[index] = { ...this.todos[index], ...updateTodo }
+        const data = this.todos[index]
+        return res.status(200).json(data)
+      }
+
+      return res.status(404).json({ message: "ID Todo not found"})
+    } catch (error) {
+      return res.status(500).json({ message: "Something error while update todo" })
+    }
+  }
+
+  delete(req: Request, res: Response) : Response {
+    try {
+      const { id } = req.params
+      const data: ITodos[] = Todos.filter(todo => todo.id !== parseInt(id))
+
+      return res.status(200).json(data)
+    } catch (error) {
+      return res.status(500).json({ message: "Something error while delete todo" })
+    }
+  }
+}
+
