530c65b456d7fda808e860efe6d57722978ccd82
Torao-Law <dandisyahrullah.12@gmail.com>
2023-09-22 10:52:08 +0700

    first commit

---
+++ src/mocks/Todos.ts
@@ -0,0 +1,21 @@
+import ITodos from "../interface/Todos";
+
+const Todos: ITodos[] = [
+  {
+    id: 1,
+    title: "Mobile legends",
+    isDone: false
+  },
+  {
+    id: 2,
+    title: "Coding",
+    isDone: true
+  },
+  {
+    id: 3,
+    title: "Mandi (optional)",
+    isDone: false
+  }
+]
+
+export default Todos;
